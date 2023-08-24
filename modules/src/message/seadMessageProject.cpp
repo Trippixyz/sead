e9af97f2bb70764859aa74671e4f30f01b21c71f
Trippixyz <trippixyz@gmail.com>
2023-08-24 04:06:00 +0200

    Add seadMessageProject

---
+++ modules/src/message/seadMessageProject.cpp
@@ -0,0 +1,30 @@
+#include <message/seadMessageProject.h>
+#include "LibMessageStudio/include/LMS_Mem.h"
+
+namespace sead
+{
+
+void MessageProject::initialize(void* prjData,sead::Heap* heap)
+{
+    // set sHeap to heap
+    sHeap = heap;
+
+    LMS_SetMemFuncs(allocForLibms_, freeForLibms_);
+
+    LMS_ProjectBinary* prjBinary = LMS_InitProject(prjData);
+
+    mProject = prjBinary;
+
+    LMS_SetMemFuncs(0, 0);
+
+    // set sHeap to 0x0
+    sHeap = 0;
+
+    s32 colorNum = LMS_GetColorNum(prjBinary);
+
+    if (colorNum > 0) {
+
+    }
+}
+
+}
