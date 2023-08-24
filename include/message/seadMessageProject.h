22dcd2c63f49f727cc2333b82e663eaefdaef38d
Trippixyz <trippixyz@gmail.com>
2023-08-24 04:01:39 +0200

    Submodule LibMessageStudio

---
+++ include/message/seadMessageProject.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <heap/seadHeap.h>
+#include "LibMessageStudio/include/LMS_Project.h"
+
+namespace sead
+{
+
+class MessageProject
+{
+public:
+    virtual ~MessageProject();
+
+    void initialize(void* prjData,sead::Heap* heap);
+    void finalize();
+    
+    // Libms => LibMessageStudio
+    static void* allocForLibms_(u64 size);
+    static void freeForLibms_(void* ptr);
+
+    LMS_ProjectBinary* getInitializeData();
+
+    LMS_ProjectBinary* mProject;
+    u32 mGradationColorCount;
+    char unk[0x34];
+
+    static Heap* sHeap;
+};
+
+}  // namespace sead
